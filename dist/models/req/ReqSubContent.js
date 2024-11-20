6231dd4d272abf05c64b43dc97f21338b173f11c
Mayank Gour <mayank@navigolabs.com>
2024-11-20 16:07:29 +0530

    Remove dist folder

--- dist/models/req/ReqSubContent.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// import { SubContent } from "../../entity/SubContent";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=ReqSubContent.js.map
--- dist/models/res/ResCity.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=ResCity.js.map
